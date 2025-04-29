df1d6229ad13867d3c047bb663d283d2bcde0791
AACnotAliVe <150322222+AACnotAliVe@users.noreply.github.com>
2025-04-29 11:40:15 +0300

    Create randomizer.sh

---
+++ randomizer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+BASE_DIR="$HOME/files"
+
+echo "Удаление старой директории $BASE_DIR..."
+rm -rf "$BASE_DIR"
+
+echo "Создание новой структуры в $BASE_DIR..."
+mkdir -p "$BASE_DIR/dir1/subdir"
+mkdir -p "$BASE_DIR/.hidden/.subhidden"
+
+# Случайные обычные файлы
+for i in {1..5}; do
+  tr -dc 'a-zA-Z\n ' </dev/urandom | head -c 1024 > "$BASE_DIR/file$i.txt"
+done
+
+# Случайные скрытые файлы
+for i in {1..3}; do
+  tr -dc 'a-zA-Z\n ' </dev/urandom | head -c 1024 > "$BASE_DIR/.hidden/.file$i.log"
+done
+
+# Вложенные файлы
+for i in {1..3}; do
+  tr -dc 'a-zA-Z\n ' </dev/urandom | head -c 1024 > "$BASE_DIR/dir1/subdir/file$i.txt"
+done
+
+echo "Структура создана в $BASE_DIR"
